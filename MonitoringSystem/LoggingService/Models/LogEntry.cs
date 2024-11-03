e544d1790b9184621b4e49adde4c4d792f1b8f78
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2024-11-03 16:46:25 +0100

    Added project, models, EF installation, docker setup and some configuration

---
+++ MonitoringSystem/LoggingService/Models/LogEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingService.Models
+{
+    public enum LogLevel
+    {
+        Trace,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Critical
+    }
+
+    public class LogEntry
+    {
+        public int Id { get; set; } 
+        public string Message { get; set; }
+        public LogLevel Level { get; set; } 
+        public DateTime Timestamp { get; set; }
+        public string Source { get; set; } 
+        public string Exception { get; set; } 
+        public string UserId { get; set; } 
+        public string CorrelationId { get; set; } 
+        public string TraceId { get; set; }
+        public string SpanId { get; set; }
+    }
+
+}
---
+++ MonitoringSystem/LoggingService/Models/MonitoringDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LoggingService.Models
+{
+    public class MonitoringDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MonitoringDbContext(DbContextOptions<MonitoringDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<LogEntry> LogEntries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Use the connection string from appsettings.json
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("LoggingDatabase"));
+            }
+        }
+    }
+
+}
